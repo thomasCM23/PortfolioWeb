beb8472f5394caa21a2ee1ae4346bd4612d89f3b
Thomas CM <thomas.chapados.muermans@gmail.com>
2019-02-01 09:03:25 -0500

    changed port

--- backend/main.py
+++ backend/main.py
@@ -63,4 +63,4 @@ def add_headers(response):
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
