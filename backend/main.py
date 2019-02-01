9f08ef7aec6f09ed1e12aa1914c588858524a42f
Thomas CM <thomas.chapados.muermans@gmail.com>
2019-02-01 11:19:06 -0500

    trying to figure this out

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,7 @@ class Translate(Resource):
             translated_txt = nmt.start_inference_nmt(txt_to_translate, str(uuid.uuid4()) + '.txt', args['langauge'])
             translated_txt = re.sub(r'\s([.,!?();:"](?:\s|$))', r'\1', translated_txt)
         except Exception as e:
-            translated_txt = "The language " + args['langauge'] + " has not yet been implemented" + " ERROR: " + str(e)
+            translated_txt = "The language " + args['langauge'] + " has not yet been implemented" + " ERROR: " + str(e) + "  = " + str(e.with_traceback())
 
         return {'text': translated_txt.replace("&apos;", "'")}
 
